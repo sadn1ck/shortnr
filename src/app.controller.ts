d2955dfe304800682fc1478ce819b0c721a783c1
Anik Das <anikdas0811@gmail.com>
2021-04-02 21:44:13 +0530

    feat: added config and setup 404 route
    Signed-off-by: Anik Das <anikdas0811@gmail.com>

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -5,8 +5,8 @@ import { AppService } from './app.service';
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
-  @Get()
-  getHello(): string {
-    return this.appService.getHello();
+  @Get('/404')
+  getHello() {
+    return { statusCode: 404, message: 'This URL does not exist!' };
   }
 }
---
+++ src/config/index.ts
@@ -0,0 +1,4 @@
+export default () => ({
+  baseURL: process.env.BASE_URL || 'http://localhost:3000',
+  frontendURL: process.env.FRONT_URL || 'http://localhost:5000',
+});
